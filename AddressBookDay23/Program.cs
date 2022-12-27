60dc78738ca8d4a90a356d1a7d4f285194604233
PrajaktaShewale <shewalep94@gmail.com>
2022-12-27 08:49:12 +0530

    [Prajakta]Add:Added Program to create basic contact Book

---
+++ AddressBookDay23/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AddressBookDay23
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome To Address BOOK Program");
+            AddressBook ab = new AddressBook("Prajakta","hjdhjd","abs","Mumbai","Maharashtra", "pra23@gmail.com", 400023, 9632587412);
+            ab.DisplayContacts();
+        }
+    }
+}
